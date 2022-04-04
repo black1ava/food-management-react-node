479e6105d80d878db34a49a90e4ad19017cb45e7
Roeunbora Tharath <roeunboratharath@gmail.com>
2022-04-04 09:50:07 +0700

    Add server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+const { PORT, URI } = process.env;
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+mongoose.connect(URI);
+mongoose.connection.once('open', function(){
+  console.log('Connect to database successfully');
+});
+
+app.listen(PORT, function(){
+  console.log(`Server is running on port ${ PORT }`);
+});
